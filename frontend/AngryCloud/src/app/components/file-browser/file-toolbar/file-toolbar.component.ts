8ce3779c78a0539228f7894e12f1a76db25a7862
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2025-01-12 22:35:22 +0100

    view selection

--- frontend/AngryCloud/src/app/components/file-browser/file-toolbar/file-toolbar.component.ts
+++ frontend/AngryCloud/src/app/components/file-browser/file-toolbar/file-toolbar.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-file-toolbar',
-  imports: [MatButtonToggleModule, MatIconModule],
+  imports: [MatButtonToggleModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './file-toolbar.component.html',
   styleUrl: './file-toolbar.component.scss',
 })
-export class FileToolbarComponent {}
+export class FileToolbarComponent implements OnInit {
+  view = input.required<string>();
+  selectedView: FormControl<string | null>;
+
+  constructor() {
+    this.selectedView = new FormControl<string | null>(null);
+  }
+
+  ngOnInit() {
+    this.selectedView.setValue(this.view());
+  }
+}
